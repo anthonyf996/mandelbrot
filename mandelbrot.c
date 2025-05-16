fddb4e9b6baa2c9fa601af33b3c0b0edf7ec8b84
Anthony <anthonydfarris@gmail.com>
2025-05-16 14:20:43 -0700

    Added mandelbrot.c to this git repo.

---
+++ mandelbrot.c
@@ -0,0 +1,215 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <time.h>
+#include <complex.h>
+#include <SDL2/SDL.h>
+
+#define WINDOW_TITLE "Mandelbrot"
+#define WINDOW_WIDTH 1000
+#define WINDOW_HEIGHT 800
+#define REAL_MIN -2
+#define REAL_MAX 2
+#define IMAG_MIN -2
+#define IMAG_MAX 2
+#define DEFAULT_R 0
+#define DEFAULT_G 0
+#define DEFAULT_B 0
+
+struct App {
+	SDL_Window *window;
+	SDL_Renderer *renderer;
+	double delay_time;
+} app;
+
+bool app_init(struct App *app) {
+	if (SDL_Init(SDL_INIT_VIDEO)) {
+		fprintf(stderr, "Error initializing SDL! %s\n", SDL_GetError());
+		return 1;
+	}
+	if ((app->window = SDL_CreateWindow(WINDOW_TITLE, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WINDOW_WIDTH, WINDOW_HEIGHT, 0)) == NULL) {
+		fprintf(stderr, "Error creating window! %s\n", SDL_GetError());
+		return 2;
+	}
+	if ((app->renderer = SDL_CreateRenderer(app->window, -1, 0)) == NULL) {
+		fprintf(stderr, "Error creating renderer! %s\n", SDL_GetError());
+		return 3;
+	}
+	srand((unsigned int)time(NULL));
+	return 0;
+}
+
+void app_reset(struct App *app) {
+	app->window = NULL;
+	app->renderer = NULL;
+}
+
+void app_destroy(struct App *app) {
+	SDL_DestroyRenderer(app->renderer);
+	SDL_DestroyWindow(app->window);
+	SDL_Quit();
+	app_reset(app);
+}
+
+void draw_point(struct App *app, int x, int y, Uint8 r, Uint8 g, Uint8 b) {
+		SDL_SetRenderDrawColor(app->renderer, r, g, b, 255);
+		SDL_RenderDrawPoint(app->renderer, x, y);
+		SDL_SetRenderDrawColor(app->renderer, DEFAULT_R, DEFAULT_G, DEFAULT_B, 255);
+}
+
+double linear_transform(double min, double max, double value, double limit) {
+	return min + (max - min) * value / limit;
+}
+
+unsigned int is_in_set(double x, double y) {
+	const unsigned int MAX_ITERATIONS = 700;
+	double complex c = x + y * I;
+	double complex z = 0;
+	unsigned int i = 0;
+	while (abs(z) <= 2 && i < MAX_ITERATIONS) {
+		z = z * z + c;
+		i++;
+	}
+	if (i == MAX_ITERATIONS) {
+		return 0;
+	}
+	return i;
+}
+
+void render_mandelbrot(struct App *app, double real_min, double real_max, double imag_min, double imag_max, int r, int g, int b, unsigned int *pixels) {
+	int scale = 2;
+	for (unsigned int x = 0; x < WINDOW_WIDTH; x++) {
+		for (unsigned int y = 0; y < WINDOW_HEIGHT; y++) {
+			//double x_scaled = real_min + (real_max - real_min) * x / (float)WINDOW_WIDTH;
+			//double y_scaled = imag_min + (imag_max - imag_min) * y / (float)WINDOW_HEIGHT;
+			double x_scaled = linear_transform(real_min, real_max, x, WINDOW_WIDTH);
+			double y_scaled = linear_transform(imag_min, imag_max, y, WINDOW_HEIGHT);
+			unsigned int result = is_in_set(x_scaled, y_scaled);
+			pixels[y * WINDOW_WIDTH + x] = result;
+			//draw_point(app, x, y, result * scale + rand_r % 256, result * scale + rand_g % 256, result * scale + rand_b % 256);
+			//draw_point(app, x, y, result * rand_r % 256, result * rand_g % 256, result * rand_b % 256);
+			if (result == 0) {
+				draw_point(app, x, y, 0, 0, 0);
+			} else {
+				draw_point(app, x, y, 255 - result * scale - r, 255 - result * scale - g, 255 - result * scale - b);
+			}
+		}
+	}
+}
+
+void draw_mandelbrot(struct App *app, int r, int g, int b, unsigned int *pixels) {
+	int scale = 2;
+	for (unsigned int x = 0; x < WINDOW_WIDTH; x++) {
+		for (unsigned int y = 0; y < WINDOW_HEIGHT; y++) {
+			unsigned int result = pixels[y * WINDOW_WIDTH + x];
+			if (result == 0) {
+				draw_point(app, x, y, 0, 0, 0);
+			} else {
+				draw_point(app, x, y, 255 - result * scale - r, 255 - result * scale - g, 255 - result * scale - b);
+			}
+		}
+	}
+}
+
+int main(void) {
+	double DELAY_TIME = 1000.0 / 60.0;
+	struct App app = {
+		.window = NULL,
+		.renderer = NULL,
+		.delay_time = DELAY_TIME
+	};
+	unsigned int pixels[WINDOW_HEIGHT][WINDOW_WIDTH] = {0};
+	double real_min = REAL_MIN;
+	double real_max = REAL_MAX;
+	double real_diff = REAL_MAX - REAL_MIN;
+	double imag_min = IMAG_MIN;
+	double imag_max = IMAG_MAX;
+	double imag_diff = IMAG_MAX - IMAG_MIN;
+	double real_scale = 1.0;
+	double imag_scale = 1.0;
+	int zoom_factor = 2;
+	int zoom_count = 2;
+	int rand_r = rand() % 256;
+	int rand_g = rand() % 256;
+	int rand_b = rand() % 256;
+
+	if (app_init(&app)) {
+		fprintf(stderr, "Error initializing app! %s\n", SDL_GetError());
+		app_destroy(&app);
+		return 1;
+	}
+
+
+	SDL_RenderClear(app.renderer);
+	render_mandelbrot(&app, REAL_MIN, REAL_MAX, IMAG_MIN, IMAG_MAX, rand_r, rand_g, rand_b, &pixels);
+	while (true) {
+		SDL_Event event;
+		while (SDL_PollEvent(&event)) {
+			switch(event.type) {
+				case SDL_QUIT:
+					app_destroy(&app);
+					return 0;
+					break;
+				case SDL_KEYDOWN:
+					switch (event.key.keysym.scancode) {
+						case SDL_SCANCODE_ESCAPE:
+							app_destroy(&app);
+							return 0;
+							break;
+						case SDL_SCANCODE_SPACE:
+							render_mandelbrot(&app, REAL_MIN, REAL_MAX, IMAG_MIN, IMAG_MAX, rand_r, rand_g, rand_b, &pixels);
+							real_min = REAL_MIN;
+							real_max = REAL_MAX;
+							imag_min = IMAG_MIN;
+							imag_max = IMAG_MAX;
+							real_scale = 1.0;
+							imag_scale = 1.0;
+							break;
+						case SDL_SCANCODE_C:
+							rand_r = rand() % 256;
+							rand_g = rand() % 256;
+							rand_b = rand() % 256;
+							//render_mandelbrot(&app, real_min, real_max, imag_min, imag_max, rand_r, rand_g, rand_b, &pixels);
+							draw_mandelbrot(&app, rand_r, rand_g, rand_b, &pixels);
+							break;
+						default:
+							break;
+					}
+					break;
+				case SDL_MOUSEBUTTONDOWN:
+					/*
+					for (int i = 0; i < zoom_count; i++) {
+						real_scale = linear_transform(real_min, real_max, event.button.x, WINDOW_WIDTH);
+						imag_scale = linear_transform(imag_min, imag_max, event.button.y, WINDOW_HEIGHT);
+						real_min = (real_min + real_scale) / zoom_factor;
+						real_max = (real_max + real_scale) / zoom_factor;
+						imag_min = (imag_min + imag_scale) / zoom_factor;
+						imag_max = (imag_max + imag_scale) / zoom_factor;
+					}
+					*/
+						real_scale = linear_transform(real_min, real_max, event.button.x, WINDOW_WIDTH);
+						imag_scale = linear_transform(imag_min, imag_max, event.button.y, WINDOW_HEIGHT);
+						real_diff = real_max - real_min;
+						real_min = real_scale - (real_diff / (2.0 * zoom_factor));
+						real_max = real_scale + (real_diff / (2.0 * zoom_factor));
+						//real_min /= 2.0;
+						//real_max /= 2.0;
+						imag_diff = imag_max - imag_min;
+						imag_min = imag_scale - (imag_diff / (2.0 * zoom_factor));
+						imag_max = imag_scale + (imag_diff / (2.0 * zoom_factor));
+						//imag_min /= 2.0;
+						//imag_max /= 2.0;
+					render_mandelbrot(&app, real_min, real_max, imag_min, imag_max, rand_r, rand_g, rand_b, &pixels);
+					break;
+				default:
+					break;
+			}
+		}
+		//draw_point(&app, WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2, 255, 255, 255); // Draw point at center of screen for reference
+		SDL_RenderPresent(app.renderer);
+		SDL_Delay(app.delay_time);
+	}
+	
+	app_destroy(&app);
+	return 0;
+}
